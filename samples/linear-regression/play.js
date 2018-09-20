175afa3278be53aea38cd909ad3ea4244635caed
Erol Eskici <aberec@almbrand.dk>
2018-09-20 20:53:42 +0200

    more tensor

--- samples/linear-regression/play.js
+++ samples/linear-regression/play.js
@@ -12,9 +12,9 @@ async function buildGraf() {
         yvalues[i] = Math.pow(xvalues[i], 2);
     }
     //row, col
-    const shape = [5, 2];
-    const _xvalues = tf.tensor2d(xvalues, shape);
-    const _yvalues = tf.tensor2d(yvalues, shape);
+    const shape = [,10];
+    const _xvalues = tf.tensor1d(xvalues);
+    const _yvalues = tf.tensor1d(yvalues);
 
     _xvalues.print();
     _yvalues.print();
@@ -29,7 +29,7 @@ async function buildGraf() {
     // dense is a "full connected layer"
     const hidden = tf.layers.dense({
         units: 4, // number of nodes
-        inputShape: [2], // input shape
+        inputShape: [1], // input shape
         activation: 'sigmoid'
     });
     // Add the layer
@@ -51,86 +51,19 @@ async function buildGraf() {
         optimizer: sgdOpt,
         loss: tf.losses.meanSquaredError
     });
+    
+    train(model, _xvalues, _yvalues).then(() => {
+        let outputs = model.predict(_xvalues);
+        plotDataAndPredictions('#random .plot', _xvalues, _yvalues, outputs);
 
-    train(model, xs, ys).then(() => {
-        let outputs = model.predict(xs);
         outputs.print();
         console.log('training complete');
     });
-    
-    await plotDataAndPredictions(
-        '#random .plot', _xvalues, _yvalues, predictionsBefore);
+
 }
 
 
-// Step 2. Create an optimizer, we will use this later. You can play
-// with some of these values to see how the model performs.
-const numIterations = 75;
-const learningRate = 0.5;
-const optimizer = tf.train.sgd(learningRate);
 
-// Step 3. Write our training process functions.
-
-/*
- * This function represents our 'model'. Given an input 'x' it will try and
- * predict the appropriate output 'y'.
- *
- * It is also sometimes referred to as the 'forward' step of our training
- * process. Though we will use the same function for predictions later.
- *
- * @return number predicted y value
- */
-function predict(x) {
-  // y = a * x ^ 3 + b * x ^ 2 + c * x + d
-  return tf.tidy(() => {
-    return a.mul(x.pow(tf.scalar(3, 'int32')))
-      .add(b.mul(x.square()))
-      .add(c.mul(x))
-      .add(d);
-  });
-}
-
-/*
- * This will tell us how good the 'prediction' is given what we actually
- * expected.
- *
- * prediction is a tensor with our predicted y values.
- * labels is a tensor with the y values the model should have predicted.
- */
-function loss(prediction, labels) {
-    // Having a good error function is key for training a machine learning model
-    const error = prediction.sub(labels).square().mean();
-    return error;
-  }
-
-
-/*
- * This will iteratively train our model.
- *
- * xs - training data x values
- * ys — training data y values
- */
-async function train(xs, ys, numIterations) {
-    for (let iter = 0; iter < numIterations; iter++) {
-      // optimizer.minimize is where the training happens.
-  
-      // The function it takes must return a numerical estimate (i.e. loss)
-      // of how well we are doing using the current state of
-      // the variables we created at the start.
-  
-      // This optimizer does the 'backward' step of our training process
-      // updating variables defined previously in order to minimize the
-      // loss.
-      optimizer.minimize(() => {
-        // Feed the examples into the model
-        const pred = predict(xs);
-        return loss(pred, ys);
-      });
-  
-      // Use tf.nextFrame to not block the browser.
-      await tf.nextFrame();
-    }
-  }
 
 
 
