cd5b1da2ba68460a1045b5aa76d4dc03b1420053
Erol Eskici aka mr-robot <aberec@almbrand.dk>
2018-09-27 15:11:20 +0200

    tensorflow work

--- samples/mobilenet/play.js
+++ samples/mobilenet/play.js
@@ -15,14 +15,12 @@ const mobilenetDemo = async () => {
 
   mobilenet = await tf.loadModel(MOBILENET_MODEL_PATH);
 
-  // Warmup the model. This isn't necessary, but makes the first prediction
-  // faster. Call `dispose` to release the WebGL memory allocated for the return
-  // value of `predict`.
+
   mobilenet.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
 
   status('');
 
-  // Make a prediction through the locally hosted cat.jpg.
+
   const catElement = document.getElementById('cat');
   if (catElement.complete && catElement.naturalHeight !== 0) {
     predict(catElement);
